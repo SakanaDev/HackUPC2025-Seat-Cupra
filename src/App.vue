c5a69a7231e1ad5faca36c3816ac0e065779dcdc
Halvsnick <ncd20404@gmail.com>
2025-05-03 00:51:14 +0200

    Video intro funcionant

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script>
+import VideoPro from './components/VideoPro.vue'
+export default {
+    components:{
+        VideoPro,
+    },
+    data() {
+    return {
+      endVideo: false,
+    }
+  }
+}
+
 </script>
 
 <template>
+    <VideoPro v-if="!endVideo" @video-ended="endVideo = true" />
+    <h1>test</h1>
 </template>
 
 <style scoped>
---
+++ src/components/VideoPro.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  methods: {
+    goToNextPage() {
+      this.$emit('video-ended')  // Emite evento al padre
+    },
+  }
+}
+
+</script>
+
+<template>
+    <div ref="container" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%;">
+      <video
+        ref="video"
+        autoplay
+        muted
+        playsinline
+        @ended="goToNextPage"
+        @canplay="enterFullscreen"
+        style="width: 100vw; height: 100vh; object-fit: cover; background-color: black;">
+        <source src="/img/videoplayback.mp4" type="video/mp4" />
+      </video>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
