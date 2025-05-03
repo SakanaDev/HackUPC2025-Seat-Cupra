293b1a741eb7dd1fd77d94469d3c8dd25b29e05e
roxo112475 <ng220806@gmail.com>
2025-05-03 07:03:08 +0200

    parece que funciona

--- chatbot/API.py
+++ chatbot/API.py
@@ -5,11 +5,10 @@ import pathlib
 
 client = genai.Client(api_key="AIzaSyCuDpqDuQMslHdR4HbMru2Y9DpE94w5UZg")
 
-doc_url = "https://discovery.ucl.ac.uk/id/eprint/10089234/1/343019_3_art_0_py4t4l_convrt.pdf"
+doc_url = "https://hackupc2025.slack.com/files/U08NSQU4G1Z/F08QU6K8HKM/cupra_tavascan_owners_manual_11_24_gb.pdf"
 
 # Retrieve and encode the PDF byte
-filepath = pathlib.Path('chatbot/manual_cupra.pdf')
-filepath.write_bytes(httpx.get(doc_url).content)
+filepath = pathlib.Path('manual_cupra.pdf')
 
 prompt = "eres un asistente virtual experto en ayudar a comprender manuales de usuario para nuevos clientes, responde esto: " + input("Pregunta puta: ")
 
