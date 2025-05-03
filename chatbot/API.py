2ba7f4766ef002e2bad329d70d219337ed95577a
roxo112475 <ng220806@gmail.com>
2025-05-03 09:52:04 +0200

    ordenado pre-estructura de chat

--- chatbot/API.py
+++ chatbot/API.py
@@ -4,24 +4,28 @@ import httpx
 import pathlib
 import json
 
-client = genai.Client(api_key="AIzaSyCuDpqDuQMslHdR4HbMru2Y9DpE94w5UZg")
+client = genai.Client(api_key = "AIzaSyCuDpqDuQMslHdR4HbMru2Y9DpE94w5UZg")
 
 doc_url = "https://hackupc2025.slack.com/files/U08NSQU4G1Z/F08QU6K8HKM/cupra_tavascan_owners_manual_11_24_gb.pdf"
 
 # Retrieve and encode the PDF byte
 filepath = pathlib.Path('manual_cupra.pdf')
 
+### Entrada ###
 prompt = "eres un asistente virtual experto en ayudar a comprender manuales de usuario para nuevos clientes, responde esto sobre mi CUPRA Tavascan: " + input("¿En que puedo ayudarle?\n")
 
-
+### Procesamiento de la respuesta ###
 response = client.models.generate_content(
-  model="gemini-2.0-flash",
-  contents=[
+  model = "gemini-2.0-flash",
+  contents = [
       genai.types.Part.from_bytes(
         data = filepath.read_bytes(),
         mime_type='application/pdf',
       ),
       prompt])
+
+
+### Devolucion ###
 print(response.text)
 messagge = {}
 messagge['chatbot'] = response.text
